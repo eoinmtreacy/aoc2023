1c41bf82b54e5f290eaccf08b5415810587d8d4b
eoinmtreacy <eointreacy5@gmail.com>
2023-12-02 17:06:18 +0000

    not handling duplicates

--- day1.py
+++ day1.py
@@ -4,26 +4,51 @@ lines = f.readlines()
 
 nums = []
 
-digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-letters = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+numbers = {
+    "0": "0",
+    "1": "1",
+    "2": "2",
+    "3": "3",
+    "4": "4",
+    "5": "5",
+    "6": "6",
+    "7": "7",
+    "8": "8",
+    "9": "9",
+    "zero": "0",
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+}
 
-for line in lines:
-    new = ""
+checking = list(numbers.keys())
 
-    for digit in digits:
-        if digit in line:
-            new = new + str(digits.index(digit))
-    for letter in letters:
-        if digit in line:
-            new = new + str(letters.index(letter))
-            
-    new = new[0] + new[-1]
-    nums.append(new)
+for l,line in enumerate(lines):
+    print(line)
+    new = []
+    for check in checking:
+        if check in line:
+            for i, check in enumerate(line):
+                    try:
+                        new.append((numbers[check],i))
+                    except:
+                         pass
+    new.sort(key=lambda x : x[1])
+    addition = new[0][0] + new[-1][0]
+    if l == 999:
+        pass
+    nums.append(addition)
+    print(addition)
 
 sum = 0
 
 for num in nums:
     sum += int(num)
-    print(num, end = " ")
 
 print(sum)
