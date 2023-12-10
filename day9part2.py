5585094a5eb2ec03cce5092db4cdde0fd32aeabf
eoinmtreacy <eointreacy5@gmail.com>
2023-12-10 23:09:12 +0000

    just reversed the increment to a decrement property

--- day9part2.py
+++ day9part2.py
@@ -32,10 +32,10 @@ class Line:
         if self.intervals == 0:
             return 0
         else:
-            return self.intervals.reading[-1] + self.intervals.predict_next_inc
+            return self.intervals.reading[0] - self.intervals.predict_next_inc
 
     def future_reading(self):
-        return self.reading[-1] + self.predict_next_inc
+        return self.reading[0] - self.predict_next_inc
     
 def main():
     lines = [Line(line) for line in read_file("code.txt")]
@@ -47,8 +47,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-# 1,681,758,967 too high (max != 0)
-# 1,681,758,908 w/ loop to check if all not zero
-# 891,209,680 too low (if not all)
-# 886,195,462 (sum of all = 0)
-# 1,8690,553 too low 
+# 886196572 too high
